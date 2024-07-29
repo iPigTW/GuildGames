9444fb291feece2ba8cb98e27a687237884fdf6b
iPig <diegogamingtw@gmail.com>
2024-07-29 21:39:28 +0800

    Update aiCommand.js

--- features/aiCommand.js
+++ features/aiCommand.js
@@ -9,31 +9,34 @@ new Command("aichat", (username, args) => {
     let prompt = args.join(" ");
     
     if (staff.includes(username)) {
-        request({
-            url: "https://ai.spin.rip/chat",
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: {
-                prompt: prompt,
-                name: username
-            },
-            json: true
-        })
-        .then(response => {
+        try {
+            let response = await request({
+                url: "https://ai.spin.rip/chat",
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: {
+                    prompt: prompt,
+                    name: username
+                },
+                json: true
+            })
+        
             if (response.data && response.data.response) {
                 ChatLib.command(`gc ${response.data.response}`);
             } else {
                 ChatLib.chat(Settings.chatPrefix + "&cError!");
             }
-        })
-        .catch(error => {
-            if (error.isAxiosError) {
+        }catch (error) {
+             if (error.isAxiosError) {
                 ChatLib.chat(Settings.chatPrefix + "&c" + error.code + ": " + error.response.data);
             } else {
                 ChatLib.chat(Settings.chatPrefix + "&c" + error.code + ": " + error.message);
             }
-        })
+        }
+        
+           
+        
     }
 });
